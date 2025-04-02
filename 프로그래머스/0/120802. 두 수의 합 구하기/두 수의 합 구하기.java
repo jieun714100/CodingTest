8dc7a7fa1e1681e38a04263c5221245f4d648ebf
jieun714100 <s2516@e-mirim.hs.kr>
2025-04-02 18:57:15 +0900

    [level 0] Title: 두 수의 합 구하기, Time: 0.04 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        
+        // int num1 = 2;
+        // int num2 = 3;
+            
+        int answer = num1+num2;
+        return answer; // print(answer)
+    }
+}
