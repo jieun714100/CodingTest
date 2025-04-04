b4491f324212a8afada47ea548ceeece49fcfdbe
jieun714100 <s2516@e-mirim.hs.kr>
2025-04-05 00:31:53 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -3,10 +3,7 @@ class Solution {
         int answer = 0;
         if(num1==num2){
             answer=1;
-        } else{
-            answer=-1;
-        }
-        
+        }else answer=-1;
         return answer;
     }
 }
