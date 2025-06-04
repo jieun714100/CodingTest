a0de5543258db6d76eb2be196963a1072a8c874d
정지은 <s2516@e-mirim.hs.kr>
2025-06-04 23:50:37 +0900

    [level 0] Title: 세균 증식, Time: 0.01 ms, Memory: 3.65 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n, int t) {
+    int answer = 1;
+    for(int i=0; i<t; i++){
+        n*=2;
+    
+    }
+    return n;
+}
+//2마리10시간 >2*2*2*2*2*2*2*2*2*2*2*
+//7마리 15시간> 7*2*2...
